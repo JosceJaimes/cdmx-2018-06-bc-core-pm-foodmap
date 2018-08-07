9d3f0f13570da7e70bf747c02a6331e75ddf040f
Josce Jaimes <nanayhachi94@gmail>
2018-08-07 07:12:39 -0500

    añadiendo redireccion a pagina principal

--- js/splash.js
+++ js/splash.js
@@ -1,31 +1,7 @@
-splash = ()=>{
+window.onload=timeout;
+function timeout(){
+window.setTimeout("redirect()",5000)}
 
-    var preload = document.getElementById('preload');
-    var loading = 0;
-    var id = setInterval(frame, 64);
-
-    frame = () => {
-        if ( loading == 100){
-            clearInterval(id);
-            window.open("../html/index.html", "-self");
-        } else{
-            loading = loading +1;
-            if (loading == 90) {
-                preload.style.animation = "fadeout is esase";
-            }
-        }
-    }
-}
-
-
-
-
-// window.onload=timeout;
-// timeout = () =>{
-//     window.setTimeout("redirect()",2000 )};
-//     redirect (){
-//         window.location = splash.html
-//         return;
-//     };
-
-//      onload = "timeout()"
+function redirect(){
+window.location="index.html"
+return}
