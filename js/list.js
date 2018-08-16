83682f206a6850f6a0a4f66c62bba4463d059129
Josce Jaimes <nanayhachi94@gmail>
2018-08-16 07:42:29 -0500

    Primer intento de filtrado

--- js/list.js
+++ js/list.js
@@ -1,38 +1,58 @@
-const list = (data)=>{
-    let results="";
-console.log(results);
+// const container = document.getElementById('restaurant'); 
+let btnsearch = document.getElementById('open');
 
-let restaurants = Object.values(data);
-// console.log(restaurants);
-for(i = 0; i < restaurants.length; i++){
+const list = (data)=>{
+  let results = '';
+  console.log(results);
+
+  let restaurants = Object.values(data);
+  // console.log(restaurants);
+  for (i = 0; i < restaurants.length; i++) {
     let rest = restaurants[i];
     
-    for(r of rest){
-    let ubications = (r);
+    for (r of rest) {
+      let ubications = (r);
 
-    //  console.log(ubications.img);
+      //  console.log(ubications.name);
     
-    results += `<div class="modal-body">
+      results += `<div class="modal-body">
     <div class="card">
     <div class ="ubications">
+    <p>Nombre :${ubications.name}</p>
+    <p>Tipo :${ubications.type}</p>
+    <p>Direccion :${ubications.adress}</p>
+    <p>Telefono :${ubications.phone}</p>
+    <p>Horario :${ubications.hour}</p>
     <img class="responsive-img" src="${ubications.img}">
                     </div>
     </div>
-    </div>
+                    </div>
     </div> `;
-   result.innerHTML= results;        
+      result.innerHTML = results;        
     }
-    
-}
- 
-}
+  }
+};
+// // filtrado de restaurantes
+btnsearch.addEventListener('click', e =>{    
+  window.filterRestaurants = (restaurants, search) => {
+      console.log('hol');
+      
+    if (typeof search === 'number') {
+      const filterRestaurants = [];
+    }
+    restaurants.forEach(restaurant => {
+      if (restaurant === search) {
+        filterRestaurants.push(restaurant);
+      };
+    });
+  };
+});
 
 
-
+// let image = addEventListener('img-modal').innerHtml 
 
 // // detonar el evento del modal
 // btnNext = document.getElementById('open');
-
 
 
 // const activemodal = () => { 
@@ -44,4 +64,3 @@ for(i = 0; i < restaurants.length; i++){
 // activemodal();
 
 
-console.log("hol");
